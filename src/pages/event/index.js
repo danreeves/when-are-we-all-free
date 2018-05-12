8065bd78889b93cbaed4382afd5d9a12d138c586
Daniel James Lewis <dantheorange@googlemail.com>
2018-05-12 14:33:54 +0100

    Added button to add slots to the database

--- src/pages/event/index.js
+++ src/pages/event/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { gql } from 'apollo-boost';
-import { Query } from 'react-apollo';
+import { Query, Mutation } from 'react-apollo';
 import type { Match } from 'react-router-dom';
 
 const getQuery = id => gql`
@@ -20,9 +20,35 @@ const getQuery = id => gql`
   }
 `;
 
+const CreateSlotQuery = gql`
+  mutation createSlot(
+    $eventId: ID!
+    $userId: ID!
+    $start: DateTime!
+    $end: DateTime!
+  ) {
+    createSlot(
+      data: {
+        event: { connect: { id: $eventId } }
+        user: { connect: { id: $userId } }
+        start: $start
+        end: $end
+      }
+    ) {
+      id
+      start
+      end
+      user {
+        name
+      }
+    }
+  }
+`;
+
 const getSubscription = () => gql`
   subscription {
     slot(where: { mutation_in: [CREATED, UPDATED, DELETED] }) {
+      mutation
       node {
         id
         start
@@ -64,7 +90,6 @@ const Slots = ({ eventId }: { eventId: string }) => (
   <Query query={getQuery(eventId)}>
     {({ loading, error, data, subscribeToMore }) => {
       if (loading) return <div>Loading...</div>;
-
       if (error) return <div>Error :(</div>;
 
       return (
@@ -73,6 +98,10 @@ const Slots = ({ eventId }: { eventId: string }) => (
           subscribeToMoreSlots={() => {
             subscribeToMore({
               document: getSubscription(eventId),
+              updateQuery: (prev, { subscriptionData }) => {
+                console.log(prev, subscriptionData);
+                return prev;
+              },
             });
           }}
         />
@@ -81,11 +110,40 @@ const Slots = ({ eventId }: { eventId: string }) => (
   </Query>
 );
 
+const CreateSlotButton = ({ eventId, userId }) => {
+  return (
+    <Mutation mutation={CreateSlotQuery}>
+      {createSlot => {
+        return (
+          <button
+            onClick={() => {
+              createSlot({
+                variables: {
+                  eventId,
+                  userId,
+                  start: '2018-05-07T15:13:05.475Z',
+                  end: '2018-05-07T15:13:05.475Z',
+                },
+              });
+            }}
+          >
+            Add Slot
+          </button>
+        );
+      }}
+    </Mutation>
+  );
+};
+
 export default function SlotsPage({ match }: { match: Match }) {
   return (
     <React.Fragment>
       <h1>Slots</h1>
       <Slots eventId={match.params.id} />
+      <CreateSlotButton
+        eventId={match.params.id}
+        userId="cjgtgk8gw9zq60b51010nr4at"
+      />
     </React.Fragment>
   );
 }
